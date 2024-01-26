3742fc43a07a105aee5147007fbd41999b156ea8
danniel.dias@itau-unibanco.com.br <danniel.dias@itau-unibanco.com.br>
2024-01-26 15:30:15 -0300

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzA4NA/spectral.js?branch_slug=master&token=1a423b78-7fb1-4ef2-8a4e-333e17bf0ab8";
+export default { extends: ruleset };
---
+++ docs/teste-doc.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: n9001lv321fb6
+---
+
+# teste-doc
+
+The beginning of an awesome article...
